24294c88da1ef3bef0af8fbb90d3e1e7c9556d20
Michael Kononenko <michael.kononenko101@gmail.com>
2022-12-28 01:14:42 +0200

    few fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,28 +2,41 @@ import { useState } from "react";
 import { ContactList, Filter } from "./contacts/Contacts";
 import { GlobalStyle } from "./GlobalStyle.styled";
 import { NewContactForm } from "./new-contact/NewContact";
+import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const notifyOptions = {position: 'center-top',timeout: 1500,}
 
 export const App = () => {
     const [contacts, setContacts] = useState([
-        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-        {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+        {id: nanoid(3), name: 'Rosie Simpson', number: '459-12-56'},
+        {id: nanoid(3), name: 'Hermione Kline', number: '443-89-12'},
+        {id: nanoid(3), name: 'Eden Clements', number: '645-17-79'},
+        {id: nanoid(3), name: 'Annie Copeland', number: '227-91-26'},
       ]);
     const [filter, setFilter] = useState('');
 
     const submitHandling = event =>{
       event.preventDefault();
-      const id = contacts.length + 1;
       const form = event.currentTarget;
       const name = form.elements.name.value;
       const number = form.elements.number.value;
-      setContacts(prevArray => [...prevArray, ({id: 'id-' + id, name: name, number: number})]);
+      if(contacts.find(item => item.name === name || item.number === number) ){
+           Notify.warning('Looks like you have the same note', notifyOptions);
+           return;
+        }
+      setContacts(prevArray => [...prevArray, ({id: nanoid(3), name: name, number: number})]);
+      event.currentTarget.reset();
    }
 
    const newFilter = event => setFilter(event.currentTarget.value);
    
-   const deleteContact = event => setContacts(prevArray => prevArray.filter(item => item.id !== event.currentTarget.id));
+   const deleteContact = event => {
+    const id = event.target.id;
+    const person = contacts.filter(item => item.id === id);
+    Notify.info(`${person[0].name} was deleted`, notifyOptions);
+    setContacts(prevArray => prevArray.filter(item => item.id !== id));
+   }
 
    return(
     <div style={{padding: "20px"}}>
